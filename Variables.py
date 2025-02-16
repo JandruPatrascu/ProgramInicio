a561f4a22b0a0a41a0f0e26181fc043a2012a88a
Alejandro <alejandromedina20187@gmail.com>
2025-02-16 11:30:55 -0300

    Variable_01

---
+++ Variables.py
@@ -0,0 +1,56 @@
+#Ejercicios de Variables
+
+#Ejercicio 1
+print ("name = Alejandro") 
+print ("age = 21 años") 
+print ("height = 1,73")
+
+#Ejercicio 2
+age_str = str("21")
+print("Tengo 21 años.") 
+
+#Ejercicio 3  True o False
+print ("Soy estudiante?")
+is_student = True
+#Print de la variable
+print(is_student)
+
+#Ejercicio 4 Funcion len()
+Nombre_completo = "Alejandro Medina"
+Longitud = len("nombre-completo")
+print ("La longitud del nombre es de",Longitud,"letras")
+
+#Ejercicio 5
+Nombre , Apellido , Ciudad = "Alejandro","Medina","Argentina"
+
+print("Mi nombre es",Nombre,", mi apellido es",Apellido,"y soy de",Ciudad)
+
+#Ejercicio 6 Color favorito
+color = input("¿Cuál es tu color favorito? ")
+# Color favorito
+print("Tu color favorito es el", color)
+
+#Ejercicio 7
+fruit = "Pera"
+#Print de la variable
+print("La fruta inicial es:", fruit)
+#Nueva fruta
+fruit = "naranja"
+#Nuevo valor
+print("La fruta ahora es:", fruit)
+
+# Ejercicio 8
+price = 5.99
+print(int(price))
+
+# Ejercicio 9
+
+address = "Ruta 4, Quitilipi, Chaco"
+address_len = len(address)
+print(address_len)
+
+#  Ejercicio 10
+phone: int = 606197
+print(type(phone))
+phone = 976861
+print(type(phone))
